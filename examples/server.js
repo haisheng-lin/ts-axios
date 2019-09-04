5b46086d263e47949c8e3acd0d7d4509cea9b95b
haisheng-lin <linhaicheng@xiao100.com>
2019-09-04 16:42:59 +0800

    feat: add interceptor example demo

--- examples/server.js
+++ examples/server.js
@@ -90,6 +90,10 @@ router.get('/extend/user', function(req, res) {
   })
 })
 
+router.get('/interceptor/get', function(req, res) {
+  res.end('hello!')
+})
+
 app.use(
   webpackDevMiddleware(compiler, {
     publicPath: '/__build__/',
